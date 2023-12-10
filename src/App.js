3c39c9e401769aa0611bdbf7dce9c949f7ad82f1
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2023-12-11 00:22:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import FormInput from './components/FormInput';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+  <form>
+    <FormInput placeholder="Username"/>
+    <FormInput placeholder="Email"/>
+    <FormInput
+    placeholder="Password"/>
+    <FormInput
+    placeholder="sth else"
+    />
+  </form>
     </div>
   );
-}
-
+  }
 export default App;
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,12 @@
+import "./formInput.css"
+
+const FormInput = (props)=>{
+    return(
+        <div className="formInput">
+           {/* {<label>Username</label> */}
+            <input placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default FormInput;
