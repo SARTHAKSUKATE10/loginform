be444579c07f7241773cb78af090dea92b144aa1
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2023-12-12 21:50:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import FormInput from './components/FormInput';
+import { useState } from 'react';
 
 function App() {
+
+const [username,setUsername]=useState('')
+
+console.log(username);
+
+console.log("re-rendered");
   return (
     <div className='app'>
   <form>
-    <FormInput placeholder="Username"/>
+    <FormInput placeholder="Username" setUsername={setUsername}/>
     <FormInput placeholder="Email"/>
     <FormInput
     placeholder="Password"/>
     <FormInput
     placeholder="sth else"
     />
+    <button>Submit</button>
   </form>
     </div>
   );
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,7 +4,9 @@ const FormInput = (props)=>{
     return(
         <div className="formInput">
            {/* {<label>Username</label> */}
-            <input placeholder={props.placeholder}/>
+            <input placeholder={props.placeholder}
+            onChange={e=>props.setUsername(e.target.value)}
+            />
         </div>
     )
 }
